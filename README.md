e8ad3992afd2c9780a3ceab3fe62fc2eeb31ac62
Joe D <joedef@gmail.com>
2025-10-18 19:48:07 -0400

    psistat: incr dev: near feature complete

--- README.md
+++ README.md
@@ -1,49 +1,64 @@
 # Linux Pressure Stall Information (PSI) Status App
-`psistat` is a simple python3 program to display the PSIs and to capture/display exception events.  See [PSI - Pressure Stall Information — The Linux Kernel documentation](https://docs.kernel.org/accounting/psi.html) for more information.
+`psistat` is a python3 program to display the PSIs and to capture/display exception events.  See [PSI - Pressure Stall Information — The Linux Kernel documentation](https://docs.kernel.org/accounting/psi.html) for more information.
 
 `psistat` might be found helpful when encountering unexplained system delays and you wish to identify the general cause.  Regularly occuring high PSIs may indicate the system is undersized or overloaded, and, if warranted, implementing automated load-shedding.  
 
 ## Installation
-Download the `psistat` program, make it excutable, and put in on your execution path.
-For example, this might suffice for your install:
 ```
-    git clone git@github.com:joedefen/psistat.git
-    chmod +x psistat/psistat
-    cp psistat/psistat ~/.local/bin/.
-    rm -rf psistat
+  pipx install psistat
 ```
 
 ## Instruction for Use
 Typically, simply run `psistat`.  Its window looks like:
 
 ```
-TBD
+PSIs 18:54:04 | [tT]hresh=5% [i]tvl=1 [b]rief=off [d]ump ?:help [q]uit
+ ---1s ---3s --10s --60s -300s  Full.Stall%      ---1s ---3s --10s --60s -300s  Some.Stall%
+   0.0   0.0   0.0   0.0   0.0  cpu.full           1.7   2.1   6.7   1.1   0.5  cpu.some
+   0.0   0.1   0.2   0.1   0.1  io.full            0.0   0.2   0.3   0.1   0.1  io.some
+   0.0   0.0   1.4   0.2   0.0  memory.full        0.0   0.0   1.5   0.2   0.0  memory.some
+──────────────────────────────────────────────────────────────────────────────────────────────
+000     8s: 10-18 18:53:56.099  ...................    10.50 cpu.some      >=5 i=1
+001     9s: 10-18 18:53:55.097    14.24 memory.full    14.55 memory.some   >=5 i=1
+002     9s: 10-18 18:53:55.097  ...................    40.78 cpu.some      >=5 i=1
+003    26s: 10-18 18:53:37.995    10.06 io.full        10.51 io.some       >=5 i=1
+004  1m26s: 10-18 18:52:37.665     5.86 io.full         6.08 io.some       >=5 i=1
+005  3m35s: 10-18 18:50:28.977  ...................     5.28 io.some       >=5 i=1
+006  4m35s: 10-18 18:49:28.644  ...................     5.05 io.some       >=5 i=1
 ```
 
 `psistat` displays:
-* On the top portion of the screen, the 1s, 3s, and 10s PSI stats (each is a percent stalled during the interval).
+* The first line of the screen shows some of the control keys and values.
+  * `t` decreases the event threshold by 5%; `T` increases it by 5%. Values between 5% and 95% are supported.
+  * `i` changes the event interval; if it is 10, then the only 10s average stat is watched for events. 
+  * `b` toggles whether the display makes available only as many events as viewable on the screen (no srolling)
+    or whether it allows up to 200 events that are scrollable.
+  * `d` immediately dumps to the screen all historical events (up to 1000) captured by the running program.
+    A dump will drop out of the display mode so you can copy events to another app, and allow you to display mode return when ready.
+  * `?` will show all the control and navagation keys and the current values of the controls.
+  * `q` will terminated the app.
+* On the top portion of the screen, the 1s, 3s, 10s, 60s, and 300s PSI stats (each is a percent stalled during the interval).
+  * The 60s and 300s are averages computed by the system.
 * Below the stats is a list of exception events beginning with the most recent;
   each event includes:
-  * The relative time and absolution time of the event.
-  * The type of event.
-  * The pressure value as a percentage relative to the current threshold.
-
-
-When `psistat` is running, entering these keys has special effect:
-* `j` - lower the threshold for exception events by 5% (but to no lower than 5%)
-* `k` - raise the threshold for exception events by 5% (but to no higher than 95%)
-* `q` - quit the program.
+  * A sequence number from 000 for the most recent.
+  * The relative time and absolute time of the event.
+  * For the 'full' and 'some' stats:
+    * The pressure value as a percentage relative to the current threshold.
+    * The type of event.
+  * Since the 'full' and 'some' stats are highly correlated, you will often see both in one "event" as `psistat` collects them.
 
 
 `psistat` has only a few options:
 ```
-usage: psistat [-h] [-D] [-t THRESHOLD_PCT]
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
-  -D, --debug           debug mode w/o Window
-  -t THRESHOLD_PCT, --threshold-pct THRESHOLD_PCT
-                        event threshold pct [min=5, max=95, dflt=20]
+  -t THRESHOLD_PCT, --threshold-pct THRESHOLD_PCT event threshold pct
+                      [min=5, max=95, dflt=20]
+  -b, --brief           event threshold pct [min=5, max=95, dflt=20]
+  -i EVENT_INTERVAL, --event-interval EVENT_INTERVAL event threshold pct
+                      [1, 3, 10, 60, 300]
 ```
 
 ## Load Test
--- psistat.py
+++ psistat.py
@@ -25,6 +25,7 @@ TODO List:
 # pylint: disable=invalid-name,too-many-instance-attributes,import-outside-toplevel,broad-except,too-many-locals
 
 import sys
+import os
 import time
 import re
 import traceback
@@ -58,8 +59,9 @@ def ago_str(delta_secs):
             break
         vals = (vals[1]%div, int(vals[1]/div))
         uidx += 1
-    rv = '{:d}{}'.format(vals[1], units[uidx]) if vals[1] else '    '
-    rv += '{:d}{}'.format(vals[0], units[uidx-1])
+    rv = '{:d}{}'.format(vals[1], units[uidx]) if vals[1] else ''
+    fmt = '{:02d}{}' if vals[1] else '{:2d}{}'
+    rv += fmt.format(vals[0], units[uidx-1])
     return rv
 
 
@@ -67,8 +69,7 @@ class PressureGroup:
     """Handler for one PSI group (i.e., cpu, io, or memory)
     """
     pattern = re.compile(r'^(\S+)\b.*\bavg60=(\d+\.\d+)\b.*\bavg300=(\d+\.\d+)\b.*\btotal=(\d+)$')
-    def __init__(self, debug, tag, stats):
-        self.DB = debug
+    def __init__(self, tag, stats):
         self.stats = stats
         self.avgs = {60: 0, 300: 0} # averages from /proc/pressure/{tag}
         self.tag = tag
@@ -88,8 +89,6 @@ class PressureGroup:
         allowed_subkeys = {'full', 'some'}
         for line in document.splitlines():
             match = self.pattern.match(line)
-            if self.DB:
-                print('DB:', self.tag, line)
             
             # NOTE: Your original regex assumed match was successful. Adding check.
             if not match:
@@ -112,8 +111,7 @@ class PsiStat:
     """Class to display PSI information including
     running averages and exception events"""
     singleton = None
-    def __init__(self, debug=True, threshold=20):
-        self.DB = debug
+    def __init__(self, threshold=20, brief=False):
         assert self.singleton is None
         PsiStat.singleton = self
         self.next_mono = time.monotonic_ns() # when to sleep to next
@@ -127,68 +125,80 @@ class PsiStat:
         self.events = []
         self.event_floor = {}
         self.lineno = 0 # Not strictly needed with ConsoleWindow, but harmless.
+        self.last_sample_mono = 0
 
         # --- CONSOLE-WINDOW SETUP ---
-        if not self.DB:
-            # Setup OptionSpinner for 'j' and 'k' keys
-            self.spin = OptionSpinner()
-            self.opts = self.spin.default_obj
-            self.spin.add_key('help_mode', '? - toggle help screen', vals=[False, True])
-            self.spin.add_key(attr='event_interval', descr='i - event interval',
-                      vals=[10, 3, 1, 300, 60])
-            self.spin.add_key(attr='threshold', descr='T - raise threshold',
-                      vals=list(range(5, 100, 5))) # 5, 10, 15, ..., 95)
-            self.spin.add_key(attr='lower_threshold', descr='t - lower threshold now',
-                      vals=[False, True])
-            self.spin.add_key(attr='dump_now', descr='d - dump event log now',
-                      vals=[False, True])
+        # Setup OptionSpinner for 'j' and 'k' keys
+        self.spin = OptionSpinner()
+        self.opts = self.spin.default_obj
+        self.spin.add_key('help_mode', '? - toggle help screen', vals=[False, True])
+        self.spin.add_key(attr='event_interval', descr='i - event interval (secs)',
+                  vals=[10, 3, 1, 300, 60])
+        self.spin.add_key(attr='threshold', descr='T - raise threshold (+5%)',
+                  vals=list(range(5, 100, 5))) # 5, 10, 15, ..., 95)
+        self.spin.add_key(attr='lower_threshold', descr='t - lower threshold (-5%)',
+                  vals=[False, True])
+        self.spin.add_key(attr='brief', descr='b - only load events that fit on screen',
+                  vals=['off', 'on'])
+        self.spin.add_key(attr='dump_now', descr='d - dump event log now',
+                  vals=[False, True])
+        self.spin.add_key(attr='quit', descr='q or CTRL-c - quit program',
+                  vals=[False, True])
 
-            # FIXME: keys
-            others = ''
-            other_keys = set(ord(x) for x in others)
-            other_keys.add(cs.KEY_ENTER)
-            other_keys.add(27) # ESCAPE
-            other_keys.add(10) # another form of ENTER
+        # FIXME: keys
+        others = ''
+        other_keys = set(ord(x) for x in others)
+        # other_keys.add(cs.KEY_ENTER)
+        # other_keys.add(27) # ESCAPE
+        # other_keys.add(10) # another form of ENTER
+        self.opts.brief = 'on' if brief else 'off'
 
-            # Initialize ConsoleWindow
-            self.window = ConsoleWindow(head_line=True, keys=self.spin.keys^other_keys)
-        else:
-            self.opts = SimpleNamespace(threshold=threshold,
-                                        event_interval=10)
-            self.window = None
+        # Initialize ConsoleWindow
+        self.window = ConsoleWindow(head_line=True, keys=self.spin.keys^other_keys)
 
         for tag in 'cpu', 'io', 'memory':
-            self.psgs.append(PressureGroup(debug, tag, self.stats))
+            self.psgs.append(PressureGroup(tag, self.stats))
 
 
-    def prc_samples(self):
+    def dump_event_log(self, do_return=False):
+        """  drop out of Window and dump the events """
+        if self.window:
+            self.window.stop_curses()
+        os.system('clear; stty sane')
+        for event in reversed(self.events):
+            print(event[1])
+        if do_return:
+            os.system(r' /bin/echo -e "\n\n===== Press ENTER to return to psistat ====> \c"; read FOO')
+            if self.window:
+                self.window._start_curses()
+
+
+    def prc_samples(self, valid_sample_count):
         """Show the current PSI stats/events using the
         most recent samples while also detecting new events
         for exceptional samples. Then show the recent events.
         """
-        if self.DB:
-            print(self.stats)
-        
         # Access threshold from OptionSpinner object
         threshold = self.opts.threshold
         itvl = self.opts.event_interval
+        brief = self.opts.brief
+        dt_object = datetime.fromtimestamp(time.time())
+        hhmmss = dt_object.strftime("%H:%M:%S")
 
-        if not self.DB:
-            title = f'PSISTAT | [tT]hresh={threshold}% [i]tvl={itvl} [d]ump ?:help'
-            self.window.add_header(title)
+        title = f'PSIs {hhmmss} | [tT]hresh={threshold}% [i]tvl={itvl} [b]rief={brief} [d]ump ?:help [q]uit'
+        self.window.add_header(title)
 
         # Draw Header
         header = (f' {"1s":->5} {"3s":->5} {"10s":->5} {"60s":->5} {"300s":->5}  {"Full.Stall%":>11}'
                   + f'      {"1s":->5} {"3s":->5} {"10s":->5} {"60s":->5} {"300s":->5}  {"Some.Stall%":>11}')
-        # Using add_body here since ConsoleWindow separates header content from title line
         self.window.add_header(header) 
         
         # --- Draw PSI Stats ---
-        line, event = '', ''
+        line, event, specifically = '', '', ''
         now = time.time()
-        mono_now = time.monotonic_ns()
         for idx, key in enumerate(self.stats):
             micros = self.stats[key].micros
+            del micros[valid_sample_count:]
             avgs = self.stats[key].avgs
             
             for ii in (1, 3, 10, 60, 300):
@@ -206,8 +216,6 @@ class PsiStat:
                     else:
                         pct = 100 * 1000 * delta_micros / delta_monos
                     
-                    if self.DB and delta_micros > 0:
-                        print(f'DB: {key} {delta_micros} / {delta_monos} = {pct:.9f}%')
                 else:
                     pct = avgs.get(ii, 0.0)
                 
@@ -216,38 +224,44 @@ class PsiStat:
                 # detect/collect the 1s samples that exceed the threshold
                 floor_key = f'{key}.{ii}'
                 floor = self.event_floor.get(floor_key, 0)
-                if ii == itvl and round(pct, 3) >= threshold and now >= floor:
+                if ii == itvl and round(pct, 2) >= threshold and now >= floor:
                     if not event:
                         dt_object = datetime.fromtimestamp(now)
                             # Truncate microseconds to milliseconds (.mmm) with [:-3]
                         event += dt_object.strftime("%m-%d %H:%M:%S.%f")[:-3]
 
-                    specifically = f' {key:>11s} {pct:7.3f}'
+                    specifically = f'{pct:7.2f} {key:<11s}'
                     if '.full' not in event and '.some' in key:
-                        event += ' ' * len(specifically)
-                    event += specifically
-                    if self.DB:
-                        print(event)
+                        event += '  ' + '.' * len(specifically)
+                    event += '  ' + specifically
                     self.event_floor[floor_key] = now + itvl
             line += f'  {key:<11}'
 
             if idx % 2 == 0:
                 line += '     '
             else:
-                self.window.add_header(line)
+                if not self.opts.help_mode:
+                    self.window.add_header(line)
                 line = ''
                 if event:
+                    if '.some' not in event and '.full' in key:
+                        event += '  ' + '.' * len(specifically)
                     event += f'   >={threshold} i={itvl}'
                     self.events.insert(0, (time.monotonic_ns(), event))
                     event = ''
             
-        del self.events[100:]
+        del self.events[1000:]
         
         # --- Draw Events ---
-        for event in self.events:
-            mono_ns, descr = event
-            ago = ago_str((time.monotonic_ns() - mono_ns)/BILLION).strip()
-            self.window.add_body(f'{ago:>6s}: {descr}')
+        if not self.opts.help_mode:
+            self.window.calc()
+            limit = (self.window.rows - self.window.body_base) if self.opts.brief == 'on' else 200
+            for idx, event in enumerate(self.events):
+                if idx >= limit:
+                    break
+                mono_ns, descr = event
+                ago = ago_str((time.monotonic_ns() - mono_ns)/BILLION).strip()
+                self.window.put_body(f'{idx:03d} {ago:>6s}: {descr}')
             
     def show_help_screen(self):
         """ TBD """
@@ -261,6 +275,18 @@ class PsiStat:
         for line in lines:
             self.window.put_body(line)
             
+    def count_valid_samples(self):
+        """ Valid samples are no worse that 1.25s later than the one before
+         and no more than SAMPLES """
+        # return SAMPLES-1
+        count = 1
+        del self.monos[SAMPLES:]
+        for loop in range(0, len(self.monos) - 1):
+            if self.monos[loop+1] - self.monos[loop] > 1.25:
+                break
+            count += 1
+        return count
+ 
     def process_psgs(self):
         """ TBD """
         for psg in self.psgs:
@@ -269,44 +295,36 @@ class PsiStat:
         self.times.insert(0, time.time())
         mono = time.monotonic_ns()
         self.monos.insert(0, mono)
-        del self.monos[SAMPLES:]
-        del self.times[SAMPLES:]
+        valid_sample_count = self.count_valid_samples()
+        del self.monos[valid_sample_count:]
+        del self.times[valid_sample_count:]
 
-        # Reset and render the screen content
-        if not self.DB:
+        if not self.opts.help_mode:
             self.window.clear()
             
-        self.prc_samples()
-
-        delta = self.next_mono - mono
-        while delta <= 0:
-            self.next_mono += BILLION
-            delta = self.next_mono - mono
-            
-        # Time to wait (in milliseconds)
-        return int(delta / BILLION * 1000)
+        self.prc_samples(valid_sample_count)
 
 
     def loop(self):
         """Do one loop of collecting samples, and then displaying running
         averages and exception events."""
-        timeout_ms = 1000
+        timeout_secs, min_timout_secs = 1.0, 0.9
+        
+        if time.monotonic() - self.last_sample_mono >= min_timout_secs:
+            self.process_psgs()
+            self.last_sample_mono = time.monotonic()
+
         if self.opts.help_mode:
             self.show_help_screen()
-        else:
-            timeout_ms = self.process_psgs()
-
-        if self.DB:
-            time.sleep(delta / BILLION)
-            return True # Never quit in DB mode
         
         # --- CONSOLE-WINDOW LOOP INTEGRATION ---
         
         # Render the screen
         self.window.render()
+        sleep_secs = timeout_secs - (time.monotonic() - self.last_sample_mono)
         
         # Wait for input/timeout using ConsoleWindow.prompt()
-        keystroke = self.window.prompt(seconds=timeout_ms / 1000.0)
+        keystroke = self.window.prompt(seconds=sleep_secs)
         
         if keystroke is not None:
             # Pass key to OptionSpinner for handling 'j' and 'k'
@@ -317,6 +335,11 @@ class PsiStat:
                     else:
                         self.opts.threshold = 95
                     self.opts.lower_threshold = False
+                if self.opts.dump_now:
+                    self.dump_event_log(do_return=True)
+                    self.opts.dump_now = False
+                if self.opts.quit:
+                    sys.exit(0)
 
                 return True 
             if keystroke in (ord('q'), ord('Q')):
@@ -330,16 +353,20 @@ def main():
         """Main execution function, handles CLI arguments."""
         import argparse
         parser = argparse.ArgumentParser()
-        parser.add_argument('-D', '--debug', action='store_true', help='debug mode w/o Window')
-        parser.add_argument('-t', '--threshold-pct', default=20, type=int,
+        parser.add_argument('-t', '--threshold-pct', default=20,
+                            choices=[5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95],
                             help='event threshold pct [min=5, max=95, dflt=20]')
-        args = parser.parse_args()
+        parser.add_argument('-b', '--brief', action='store_true',
+                            help='whether present only as meay events as fit on the screen')
+        parser.add_argument('-i', '--event-interval', default=10, type=int, choices=[1,3,10,60,300],
+                            help='event threshold pct')
+        opts = parser.parse_args()
         
         # Calculate nearest multiple of 5 between 5 and 95, preserving original logic
-        pct = int(round(args.threshold_pct / 5))*5
+        pct = int(round(opts.threshold_pct / 5))*5
         pct = min(max(pct, 5), 95)
 
-        pstall = PsiStat(debug=args.debug, threshold=pct)
+        pstall = PsiStat(threshold=pct, brief=opts.brief)
 
         while pstall.loop():
             pass
@@ -352,7 +379,6 @@ def main():
 
         print("exception:", str(exc))
         print(traceback.format_exc())
-
 
 if __name__ == '__main__':
 
